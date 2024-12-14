88cd66d063570ecc717a7254023dff8017c06226
dakShh <daksh.dk207@gmail.com>
2024-12-13 20:26:22 -0500

    initial setup

---
+++ lib/ai/index.ts
@@ -0,0 +1,14 @@
+import { openai } from '@ai-sdk/openai';
+import { groq } from '@ai-sdk/groq';
+
+import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
+
+import { customMiddleware } from './custom-middleware';
+
+export const customModel = (apiIdentifier: string) => {
+    return wrapLanguageModel({
+        // model: openai(apiIdentifier),
+        model: groq('gemma2-9b-it'),
+        middleware: customMiddleware,
+    });
+};
---
+++ lib/ai/prompts.ts
@@ -0,0 +1,31 @@
+// export const blocksPrompt = `
+//   Blocks is a special user interface mode that helps users with writing, editing, and other content creation tasks. When block is open, it is on the right side of the screen, while the conversation is on the left side. When creating or updating documents, changes are reflected in real-time on the blocks and visible to the user.
+
+//   This is a guide for using blocks tools: \`createDocument\` and \`updateDocument\`, which render content on a blocks beside the conversation.
+
+//   **When to use \`createDocument\`:**
+//   - For substantial content (>10 lines)
+//   - For content users will likely save/reuse (emails, code, essays, etc.)
+//   - When explicitly requested to create a document
+
+//   **When NOT to use \`createDocument\`:**
+//   - For informational/explanatory content
+//   - For conversational responses
+//   - When asked to keep it in chat
+
+//   **Using \`updateDocument\`:**
+//   - Default to full document rewrites for major changes
+//   - Use targeted updates only for specific, isolated changes
+//   - Follow user instructions for which parts to modify
+
+//   Do not update document right after creating it. Wait for user feedback or request to update it.
+//   `;
+
+// ** Custom AI prompt
+
+export const blocksPrompt = `You are a Medical AI Assessment chatbot that assists by answering all medical related questions. please ask all the questions necessary to assist and diagnose the medical problem user is facing. Only Answer user queries about medical related questions. Do not answer questions unrelated to medical questions. If the question is out of scope, respond with: "I am sorry, I can only answer medical related questions."`;
+
+// You are a friendly assistant! Keep your responses concise and helpful.
+export const regularPrompt = '';
+
+export const systemPrompt = `${blocksPrompt}`;
